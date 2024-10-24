b503cc1c46d5ed30325caf3ad9f292775e385f8e
nadarajan210 <jayakarthikkumar2002@gmail.com>
2024-10-24 19:39:21 +0530

    back to normal

--- application_main.py
+++ application_main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         sys.exit(-1)
 
     job_run_env = sys.argv[1]
-    print("Again changes are made")
+
     print("Creating Spark Session")
     spark = Utils.get_spark_session(job_run_env)
 
